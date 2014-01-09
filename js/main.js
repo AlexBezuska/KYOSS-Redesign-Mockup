9c7cd333bb986857dfb80f6d1596af50c01d32c6
Alex Bezuska <abezuska@gmail.com>
2014-01-09 18:21:30 -0500

    First Commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+
+
+for(var i=0; i<6;i++){
+    $('#posts').append("<div class='post'> <h1 class='postTitle'>Post Title </h1> <p class='postDate'>Wednesday, January 8, 2014</p> <p class='subtitle'>Location Location</p> <p class='postBody'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at felis lacus, et scelerisque tortor. Praesent ornare massa at tortor ultricies bibendum viverra odio pretium. Sed et turpis eget orci tempus vestibulum. Nunc vel lectus ut ligula facilisis eleifend sit amet et massa. Ut tellus metus, auctor sed venenatis et, feugiat sit amet risus. Nullam odio lacus, dictum sit amet venenatis quis, egestas a quam. Suspendisse molestie quam ligula, eget molestie massa. Phasellus egestas dapibus ligula id pretium. Nullam ut sapien vel lorem suscipit pretium nec at lorem. </p> </div> <!-- .post -->");
+}
